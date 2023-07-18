c0645ef98c274834408e07b8ccf6c89ebcd7661b
Omkar Lende <139203775+OmkarLende@users.noreply.github.com>
2023-07-18 09:24:55 +0530

    Update DemoProject.java
    Made Some Changes in  Java Code

--- Demo/src/DemoProject.java
+++ Demo/src/DemoProject.java
@@ -3,6 +3,7 @@ public class DemoProject {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		System.out.println("Welcome to GitHub");
+		System.out.println("I am making changes");
 		
 	}
 
